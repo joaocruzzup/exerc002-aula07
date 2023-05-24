071d96712848a79cd6542f5516d713842d8ca06f
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-05-24 10:53:29 -0300

    Adicionado ex01 e ex02

---
+++ src/ex02/CalculadoraMedia.java
@@ -0,0 +1,47 @@
+package ex02;
+
+import java.util.Scanner;
+
+public class CalculadoraMedia {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("-------------------------------------------------");
+        System.out.println("Boas-vindas à Calculadora de Médias Aritméticas!");
+        System.out.println("-------------------------------------------------");
+
+        System.out.println("Digite sua 1ª nota: ");
+        double nota1 = sc.nextDouble();
+        System.out.println("Digite sua 2ª nota: ");
+        double nota2 = sc.nextDouble();
+        System.out.println("Digite sua 3ª nota: ");
+        double nota3 = sc.nextDouble();
+        System.out.println("Digite sua 4ª nota: ");
+        double nota4 = sc.nextDouble();
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.printf("Sua média é: %.2f %n", media);
+        System.out.println("-------------------------------------------------");
+
+
+        System.out.println("Você gostaria de saber se foi aprovado? \nDigite 'Y' para SIM ou 'N' para NÃO");
+        String resposta = sc.next().toUpperCase();
+
+        if (resposta.equals("Y")) {
+            System.out.println("Digite a média de corte na sua instituição:");
+            double mediaCorte = sc.nextDouble();
+            if (media >= mediaCorte){
+                System.out.printf("Parabéns! Como sua média foi %.2f, Você foi aprovado! %n", media, mediaCorte);
+                System.out.println("Programa finalizado.");
+            } else {
+                System.out.println("Infelizmente com essas notas você está reprovado :/");
+                System.out.println("Programa finalizado.");
+            }
+        }
+        else {
+            System.out.println("Tudo bem!");
+            System.out.println("Programa finalizado.");
+        }
+    }
+}
